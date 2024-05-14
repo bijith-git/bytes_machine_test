5e764763f1040d49580e10912e0a5f82ed826291
bijith <bijithpn@gmail.com>
2024-05-14 10:01:13 +0530

    Update Home and HomeItemWidget with RefreshIndicator, BouncingScrollPhysics, and MasonryGridView
    The changes include:
    
    * Wrapping the ListView in RefreshIndicator in Home widget to enable pull-to-

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -14,20 +14,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late ScrollController scrollController;
+  ScrollController scrollController = ScrollController();
 
   @override
   void dispose() {
-    scrollController.removeListener(() =>
-        Provider.of<ProductProvider>(context, listen: false)
-            .loadMoreProduct(scrollController));
+    scrollController.removeListener(() {
+      Provider.of<ProductProvider>(context, listen: false)
+          .loadMoreProduct(scrollController);
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.position.pixels) {
+        print('scroll down');
+      }
+    });
     super.dispose();
   }
 
   @override
   void initState() {
     super.initState();
-    scrollController = ScrollController();
+    scrollController.addListener(() =>
+        Provider.of<ProductProvider>(context, listen: false)
+            .loadMoreProduct(scrollController));
   }
 
   List<Map<String, dynamic>> bottomNaviationList = [
@@ -40,8 +47,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Consumer<ProductProvider>(builder: (context, productProvider, _) {
-      scrollController
-          .addListener(() => productProvider.loadMoreProduct(scrollController));
       return Scaffold(
           appBar: AppBar(
             elevation: 0,
@@ -73,98 +78,90 @@ class _HomeState extends State<Home> {
                 return BottomNavigationBarItem(
                     icon: Icon(nav['icon']), label: nav['label']);
               })),
-          body: Center(
-            child: productProvider.state == AppState.loading
-                ? const CircularProgressIndicator()
-                : productProvider.state == AppState.error
-                    ? Text(productProvider.errorMessage)
-                    : RefreshIndicator(
-                        onRefresh: () => productProvider.getProduct(),
-                        child: ListView(
-                          controller: scrollController,
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 15),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      child: TextFormField(
-                                    decoration: InputDecoration(
-                                        constraints:
-                                            const BoxConstraints(maxHeight: 50),
-                                        prefixIcon: const Icon(Icons.search),
-                                        border: OutlineInputBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(7)),
-                                        hintText: "Search your product",
-                                        contentPadding:
-                                            const EdgeInsets.symmetric(
-                                                horizontal: 15, vertical: 10)),
-                                  )),
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                  ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                          minimumSize: const Size(50, 50),
-                                          backgroundColor: primary,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(7)),
-                                          padding: EdgeInsets.zero),
-                                      onPressed: () {},
-                                      child: const Icon(
-                                        Icons.tune,
-                                        color: Colors.white,
-                                      ))
-                                ],
+          body: productProvider.state == AppState.loading
+              ? const Center(child: CircularProgressIndicator())
+              : productProvider.state == AppState.error
+                  ? Center(child: Text(productProvider.errorMessage))
+                  : ListView(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      controller: scrollController,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: TextFormField(
+                                decoration: InputDecoration(
+                                    constraints:
+                                        const BoxConstraints(maxHeight: 50),
+                                    prefixIcon: const Icon(Icons.search),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(7)),
+                                    hintText: "Search your product",
+                                    contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 10)),
+                              )),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      minimumSize: const Size(50, 50),
+                                      backgroundColor: primary,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(7)),
+                                      padding: EdgeInsets.zero),
+                                  onPressed: () {},
+                                  child: const Icon(
+                                    Icons.tune,
+                                    color: Colors.white,
+                                  ))
+                            ],
+                          ),
+                        ),
+                        MasonryGridView.count(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 25),
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 27,
+                            crossAxisSpacing: 23,
+                            itemCount: productProvider.productList.length,
+                            itemBuilder: (context, index) {
+                              var product = productProvider.productList[index];
+                              return HomeItemWidget(
+                                product: product,
+                              );
+                            }),
+                        if (productProvider.state == AppState.loadMore)
+                          const Padding(
+                            padding: EdgeInsets.only(top: 10, bottom: 40),
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
+                        if (!productProvider.hasNextPage)
+                          Container(
+                            padding: const EdgeInsets.only(top: 30, bottom: 40),
+                            color: primary,
+                            child: Center(
+                              child: Text(
+                                'You have fetched all of the product',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge!
+                                    .copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
                               ),
                             ),
-                            MasonryGridView.count(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10, vertical: 20),
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                crossAxisCount: 2,
-                                mainAxisSpacing: 27,
-                                crossAxisSpacing: 23,
-                                itemCount: productProvider.productList.length,
-                                itemBuilder: (context, index) {
-                                  var product =
-                                      productProvider.productList[index];
-                                  return HomeItemWidget(
-                                    product: product,
-                                  );
-                                }),
-                            if (productProvider.state == AppState.loadMore)
-                              const Padding(
-                                padding: EdgeInsets.only(top: 10, bottom: 40),
-                                child: Center(
-                                  child: CircularProgressIndicator(),
-                                ),
-                              ),
-                            if (!productProvider.hasNextPage)
-                              Container(
-                                padding:
-                                    const EdgeInsets.only(top: 30, bottom: 40),
-                                color: primary,
-                                child: Center(
-                                  child: Text(
-                                    'You have fetched all of the product',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-          ));
+                          ),
+                      ],
+                    ));
     });
   }
 }
--- lib/views/widget/home_item.dart
+++ lib/views/widget/home_item.dart
@@ -40,100 +40,103 @@ class _HomeItemWidgetState extends State<HomeItemWidget> {
       elevation: 3,
       color: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              CachedNetworkImage(
-                imageUrl: widget.product.image,
-                imageBuilder: (context, imageProvider) => Container(
-                  height: 120.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(5),
-                          topRight: Radius.circular(5)),
-                      image: DecorationImage(
-                          image: imageProvider, fit: BoxFit.cover)),
-                ),
-                progressIndicatorBuilder: (context, url, progress) =>
-                    const SizedBox(
-                        height: 120.0,
-                        width: double.infinity,
-                        child: Center(child: CircularProgressIndicator())),
-                errorWidget: (context, error, stackTrace) =>
-                    const Center(child: Icon(Icons.error)),
-              ),
-              Positioned(
-                top: 5.0,
-                right: 5.0,
-                child: IconButton(
-                  style: IconButton.styleFrom(backgroundColor: Colors.white),
-                  icon: Icon(
-                    isFavorite ? Icons.favorite : Icons.favorite_border,
-                    color: primary,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      isFavorite = !isFavorite;
-                    });
-                  },
-                ),
-              ),
-              Positioned(
-                top: 5.0,
-                left: 5.0,
-                child: IconButton(
-                  style: IconButton.styleFrom(backgroundColor: Colors.white),
-                  icon: Icon(
-                    getCategoryIcon(widget.product.type),
-                    color: primary,
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(minHeight: 225),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                const SizedBox(height: 10.0),
-                Text(
-                  widget.product.title,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(fontWeight: FontWeight.bold),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+                CachedNetworkImage(
+                  imageUrl: widget.product.image,
+                  imageBuilder: (context, imageProvider) => Container(
+                    height: 120.0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(5),
+                            topRight: Radius.circular(5)),
+                        image: DecorationImage(
+                            image: imageProvider, fit: BoxFit.cover)),
+                  ),
+                  progressIndicatorBuilder: (context, url, progress) =>
+                      const SizedBox(
+                          height: 120.0,
+                          width: double.infinity,
+                          child: Center(child: CircularProgressIndicator())),
+                  errorWidget: (context, error, stackTrace) =>
+                      const Center(child: Icon(Icons.error)),
                 ),
-                const SizedBox(height: 5.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "₹${widget.product.price}",
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          letterSpacing: .5,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
+                Positioned(
+                  top: 5.0,
+                  right: 5.0,
+                  child: IconButton(
+                    style: IconButton.styleFrom(backgroundColor: Colors.white),
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: primary,
                     ),
-                    IconButton(
-                        onPressed: () =>
-                            Utils.showSnackbar(context, "Item added to cart"),
-                        icon:
-                            const Icon(Icons.add_shopping_cart, color: primary))
-                  ],
+                    onPressed: () {
+                      setState(() {
+                        isFavorite = !isFavorite;
+                      });
+                    },
+                  ),
+                ),
+                Positioned(
+                  top: 5.0,
+                  left: 5.0,
+                  child: IconButton(
+                    style: IconButton.styleFrom(backgroundColor: Colors.white),
+                    icon: Icon(
+                      getCategoryIcon(widget.product.type),
+                      color: primary,
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10.0),
+                  Text(
+                    widget.product.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 5.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "₹${widget.product.price}",
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            letterSpacing: .5,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      IconButton(
+                          onPressed: () =>
+                              Utils.showSnackbar(context, "Item added to cart"),
+                          icon: const Icon(Icons.add_shopping_cart,
+                              color: primary))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
